fa5dfe0311a11df41083223eeec8d8d4ff566adb
hieuhieu334 <151918550+hieuhieu334@users.noreply.github.com>
2023-11-24 16:30:05 +0700

    Add files via upload

---
+++ nonglamngu/content.php
@@ -0,0 +1,131 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Khoa Nông - Lâm - Ngư | Trường Đại học Hùng Vương</title>
+	<meta name="theme-color" content="#004f9e">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="Author" content="www.hvu.edu.vn">
+	<meta property="fb:pages" content="258806827498515">
+	<meta property="og:site_name" content="Trường Đại học Hùng Vương">
+	<meta property="og:image" content="https://www.hvu.edu.vn/file/1/logo.png">
+	<meta property="og:title" content="Khoa Nông - Lâm - Ngư | Trường Đại học Hùng Vương">
+	<meta property="og:url" content="https://www.hvu.edu.vn">
+	<script src="https://www.hvu.edu.vn/js/library.js"></script>
+	<script src="https://www.hvu.edu.vn/js/ticker.js"></script>
+	<script src="https://www.hvu.edu.vn/js/jquery.min.js"></script>
+	<script src="https://www.hvu.edu.vn/js/bootstrap.min.js"></script>
+	<script src="https://www.hvu.edu.vn/js/jquery.fancybox.js"></script>
+	<script src="https://www.hvu.edu.vn/js/jquery.ui.js"></script>
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery.smartmenus.bootstrap.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/fontawesome.min.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/light.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery.fancybox.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery-ui.css?v=20230717">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/style.css?v=20230717">
+	<script src="https://www.hvu.edu.vn/js/animatedcollapse.js"></script>
+	<script src="https://www.hvu.edu.vn/js/vquery.js"></script>
+    <style>.center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 20%;
+      }</style>
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div id="container">
+	<div id="" class="">
+		<img src="../nonglamngu/img/tải xuống.png" alt="Trường Đại học Hùng Vương" class="center">
+	</div>
+<div id="menu-wrap" class="clearfix">
+	<div class="navbar navbar-default" role="navigation">
+		<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		</button>
+		</div>
+		<div class="navbar-collapse collapse">
+		<ul class="nav navbar-nav">
+		<li><a href="" title="TRANG CHỦ">TRANG CHỦ</a></li>
+		
+		</ul>
+		</div>
+	</div>
+</div>
+	<div id="h-top">
+		<div id="head-search" class="clearfix">
+			<div id="search-form"><form name="search" method="post" action=""><input type="text" name="keyword" placeholder="Tìm kiếm" id="search-input"></form></div>
+		
+	<div id="wrapdiv" class="clearfix">
+		<div id="maxsize">
+	<div id="outline">
+		<div id="nav"><a href=""><i class="fal fa-home"></i> Trang chủ</a> <span>/</span>
+		<h1 class="c">Khoa Nông - Lâm - Ngư</h1>
+		<div class="pad3" id="detail"><p style="text-align: center;"><strong>NÔNG &ndash; LÂM &ndash; NGƯ</strong></p>
+<p style="text-align: center;"><strong><img src="../nonglamngu/img/khoa NLN (1).jpg" alt="" width="650" height="366" /></strong></p>
+<p style="text-align: center;"><iframe src="//www.youtube.com/embed/wfq-ePHvoZQ" width="650" height="450"></iframe><img src="../nonglamngu/img/khoa NLN (2).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (4).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (5).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (6).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (7).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (8).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (9).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (10).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (11).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (12).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/KHOA.jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (13).jpg" alt="" width="650" height="366" /></p>
+<p style="text-align: left;">&nbsp;</p>
+<p style="text-align: left;">&nbsp;</p></div>
+		<div id="date" class="fb-like" data-href="" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
+	</div>
+	</div>
+	</div>
+	<div id="foot">
+		<div id="footer">
+			<span><strong>CỔNG THÔNG TIN ĐIỆN TỬ TRƯỜNG ĐẠI HỌC HÙNG VƯƠNG</strong>
+
+			<div id="powered">
+				<div class="mrgt5"><a href="https://www.facebook.com/daihochungvuong/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 512 512"><style>svg{fill:#004f9e}</style><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg></a> 
+				<a href="https://www.youtube.com/@daihochungvuong1486/videos" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 576 512"><style>svg{fill:#004f9e}</style><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg></a></span></div>
+			</div>
+		</div>
+	</div>
+	<!-- /footer -->
+</div>
+<!-- /base -->
+
+
+<script>
+$(document).ready(function(){
+  if (screen.width <= 720) {
+$('#online-support').hide();
+}
+else
+{
+ $('#online-support').show();
+}
+  $('.support-icon-right h3').click(function(e){
+    e.stopPropagation();
+    $('#online-support').slideToggle();
+  });
+  $('#online-support').click(function(e){
+    e.stopPropagation();
+  });
+  $(document).click(function(){
+    $('#online-support').slideUp();
+  });
+});
+</script>
+<div class="support-icon-right">
+	<h3>HỖ TRỢ TRỰC TUYẾN</h3>
+	<div id="online-support">
+		<div class="fb-page" data-tabs="messages" data-href="https://www.facebook.com/nonglamnguHVU/" data-width="250" data-height="250" data-hide-cover="false" data-show-facepile="false" data-show-posts="false" data-small-header="true" data-adapt-container-width="false">&nbsp;</div>
+	</div>
+</div>
+</body>
+</html>
