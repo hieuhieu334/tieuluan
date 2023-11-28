aab2da64c9d2bb9e4b7d615d6eeed7ae2e9cf73d
hieuhieu334 <151918550+hieuhieu334@users.noreply.github.com>
2023-11-28 20:42:57 +0700

    2.0

--- nonglamngu/content.php
+++ nonglamngu/content.php
@@ -3,40 +3,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Khoa Nông - Lâm - Ngư | Trường Đại học Hùng Vương</title>
-	<meta name="theme-color" content="#004f9e">
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="Author" content="www.hvu.edu.vn">
-	<meta property="fb:pages" content="258806827498515">
-	<meta property="og:site_name" content="Trường Đại học Hùng Vương">
-	<meta property="og:image" content="https://www.hvu.edu.vn/file/1/logo.png">
-	<meta property="og:title" content="Khoa Nông - Lâm - Ngư | Trường Đại học Hùng Vương">
-	<meta property="og:url" content="https://www.hvu.edu.vn">
-	<script src="https://www.hvu.edu.vn/js/library.js"></script>
-	<script src="https://www.hvu.edu.vn/js/ticker.js"></script>
-	<script src="https://www.hvu.edu.vn/js/jquery.min.js"></script>
-	<script src="https://www.hvu.edu.vn/js/bootstrap.min.js"></script>
-	<script src="https://www.hvu.edu.vn/js/jquery.fancybox.js"></script>
-	<script src="https://www.hvu.edu.vn/js/jquery.ui.js"></script>
-	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery.smartmenus.bootstrap.css">
-	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/fontawesome.min.css">
-	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/light.css">
-	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery.fancybox.css">
-	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery-ui.css?v=20230717">
-	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/style.css?v=20230717">
-	<script src="https://www.hvu.edu.vn/js/animatedcollapse.js"></script>
-	<script src="https://www.hvu.edu.vn/js/vquery.js"></script>
     <style>.center {
         display: block;
         margin-left: auto;
         margin-right: auto;
         width: 20%;
-      }</style>
+		 }
+		 body {
+  background-image: url('../img/nonglam.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
+}
+	  </style>
 </head>
 <body>
+	
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -47,7 +29,7 @@
 }(document, 'script', 'facebook-jssdk'));</script>
 <div id="container">
 	<div id="" class="">
-		<img src="../nonglamngu/img/tải xuống.png" alt="Trường Đại học Hùng Vương" class="center">
+		<img src="../img/tải xuống.png" alt="Trường Đại học Hùng Vương" class="center">
 	</div>
 <div id="menu-wrap" class="clearfix">
 	<div class="navbar navbar-default" role="navigation">
@@ -76,9 +58,41 @@
 	<div id="outline">
 		<div id="nav"><a href=""><i class="fal fa-home"></i> Trang chủ</a> <span>/</span>
 		<h1 class="c">Khoa Nông - Lâm - Ngư</h1>
-		<div class="pad3" id="detail"><p style="text-align: center;"><strong>NÔNG &ndash; LÂM &ndash; NGƯ</strong></p>
-<p style="text-align: center;"><strong><img src="../nonglamngu/img/khoa NLN (1).jpg" alt="" width="650" height="366" /></strong></p>
-<p style="text-align: center;"><iframe src="//www.youtube.com/embed/wfq-ePHvoZQ" width="650" height="450"></iframe><img src="../nonglamngu/img/khoa NLN (2).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (4).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (5).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (6).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (7).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (8).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (9).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (10).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (11).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (12).jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/KHOA.jpg" alt="" width="650" height="366" /><img src="../nonglamngu/img/khoa NLN (13).jpg" alt="" width="650" height="366" /></p>
+
+		<div class="pad3" id="detail">
+<p style="text-align: center;">
+	<strong>NÔNG &ndash; LÂM &ndash; NGƯ</strong></p>
+<p style="text-align: center;">
+	<strong>Khoa Nông lâm ngư là một trong 9 khoa của Trường Đại học Hùng Vương, được thành lập vào ngày 10 tháng 10 năm 2007. Khoa có nhiệm vụ đào tạo nguồn nhân lực chất lượng cao, đáp ứng nhu cầu phát triển của ngành nông nghiệp, lâm nghiệp và thủy sản của tỉnh Phú Thọ và khu vực trung du miền núi phía Bắc.
+Khoa đào tạo 5 ngành học:
+Nông học,Chăn nuôi,Thú y,Lâm nghiệp,Thủy sản</strong></p>
+<p style="text-align: center;">
+	<strong><img src="../img/khoa NLN (1).jpg" alt="" width="650" height="366" /></strong></p>
+<p style="text-align: center;">
+	<iframe src="//www.youtube.com/embed/wfq-ePHvoZQ" width="650" height="450"></iframe>
+	
+<img src="../img/khoa NLN (2).jpg" alt="" width="650" height="366" /> <br>
+<em>Các ngành học của khoa được đào tạo theo định hướng ứng dụng, chú trọng thực hành và trải nghiệm thực tế. Sinh viên khoa được trang bị kiến thức chuyên môn vững vàng, kỹ năng thực hành thành thạo, khả năng thích ứng cao với môi trường làm việc thực tế.</em>
+<img src="../img/khoa NLN (4).jpg" alt="" width="650" height="366" /><br>
+<em>Khoa Nông lâm ngư có đội ngũ giảng viên giàu kinh nghiệm, tâm huyết với nghề. Các giảng viên của khoa đều là những nhà khoa học, chuyên gia đầu ngành, có trình độ chuyên môn cao, nhiệt tình trong công tác giảng dạy và nghiên cứu khoa học.</em>
+<img src="../img/khoa NLN (5).jpg" alt="" width="650" height="366" /><br>
+<em>Khoa Nông lâm ngư có cơ sở vật chất hiện đại, đáp ứng nhu cầu học tập và nghiên cứu của sinh viên. Khoa có các phòng học, phòng thí nghiệm, phòng thực hành được trang bị đầy đủ trang thiết bị hiện đại.</em>
+<img src="../img/khoa NLN (6).jpg" alt="" width="650" height="366" /><br>
+<em>Khoa Nông lâm ngư có mối quan hệ hợp tác chặt chẽ với các doanh nghiệp, đơn vị trong ngành nông nghiệp, lâm nghiệp và thủy sản. Đây là cơ hội để sinh viên của khoa được tiếp cận với thực tiễn, nâng cao kỹ năng nghề nghiệp và sớm có việc làm sau khi tốt nghiệp.</em>
+<img src="../img/khoa NLN (7).jpg" alt="" width="650" height="366" /><br>
+<em>Sinh viên khoa Nông lâm ngư năng động, sáng tạo, tích cực tham gia các hoạt động ngoại khóa, phong trào của nhà trường và xã hội.</em>
+<img src="../img/khoa NLN (8).jpg" alt="" width="650" height="366" /><br>
+<em>Khoa Nông lâm ngư đã đào tạo ra nhiều thế hệ sinh viên giỏi, có năng lực và phẩm chất đạo đức tốt, đóng góp tích cực cho sự phát triển của ngành nông nghiệp, lâm nghiệp và thủy sản của tỉnh Phú Thọ và khu vực trung du miền núi phía Bắc.
+
+</em>
+<img src="../img/khoa NLN (9).jpg" alt="" width="650" height="366" /><br>
+<em>Khoa Nông lâm ngư đang nỗ lực phấn đấu trở thành một trong những khoa hàng đầu trong lĩnh vực đào tạo nguồn nhân lực chất lượng cao cho ngành nông nghiệp, lâm nghiệp và thủy sản của Việt Nam.</em>
+<img src="../img/khoa NLN (10).jpg" alt="" width="650" height="366" /><br>
+<em></em>
+<img src="../img/khoa NLN (11).jpg" alt="" width="650" height="366" />
+<img src="../img/khoa NLN (12).jpg" alt="" width="650" height="366" />
+<img src="../img/KHOA.jpg" alt="" width="650" height="366" />
+<img src="../img/khoa NLN (13).jpg" alt="" width="650" height="366" /></p>
 <p style="text-align: left;">&nbsp;</p>
 <p style="text-align: left;">&nbsp;</p></div>
 		<div id="date" class="fb-like" data-href="" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true"></div>
@@ -87,10 +101,10 @@
 	</div>
 	<div id="foot">
 		<div id="footer">
-			<span><strong>CỔNG THÔNG TIN ĐIỆN TỬ TRƯỜNG ĐẠI HỌC HÙNG VƯƠNG</strong>
+			<span><strong>CỔNG THÔNG TIN ĐIỆN TỬ </strong>
 
 			<div id="powered">
-				<div class="mrgt5"><a href="https://www.facebook.com/daihochungvuong/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 512 512"><style>svg{fill:#004f9e}</style><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg></a> 
+				<div class="mrgt5"><a href="https://www.facebook.com/nonglamnguHVU/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 512 512"><style>svg{fill:#004f9e}</style><path d="M504 256C504 119 393 8 256 8S8 119 8 256c0 123.78 90.69 226.38 209.25 245V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.28c-30.8 0-40.41 19.12-40.41 38.73V256h68.78l-11 71.69h-57.78V501C413.31 482.38 504 379.78 504 256z"/></svg></a> 
 				<a href="https://www.youtube.com/@daihochungvuong1486/videos" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 576 512"><style>svg{fill:#004f9e}</style><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg></a></span></div>
 			</div>
 		</div>
--- nonglamngu/layout/login.php
+++ nonglamngu/layout/login.php
@@ -2,15 +2,28 @@
 <html lang="en">
 
 <head>
-  <title>Title</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS v5.2.1 -->
-  <link href="layout/phan_css" rel="stylesheet"
-    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+<title>Khoa Nông - Lâm - Ngư </title>
+	<meta name="theme-color" content="#004f9e">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta property="fb:pages" content="258806827498515">
+	<meta property="og:url" content="https://www.hvu.edu.vn">
+	<script src="https://www.hvu.edu.vn/js/library.js"></script>
+	<script src="https://www.hvu.edu.vn/js/ticker.js"></script>
+	<script src="https://www.hvu.edu.vn/js/jquery.min.js"></script>
+	<script src="https://www.hvu.edu.vn/js/bootstrap.min.js"></script>
+	<script src="https://www.hvu.edu.vn/js/jquery.fancybox.js"></script>
+	<script src="https://www.hvu.edu.vn/js/jquery.ui.js"></script>
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery.smartmenus.bootstrap.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/fontawesome.min.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/light.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery.fancybox.css">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/jquery-ui.css?v=20230717">
+	<link rel="stylesheet" href="https://www.hvu.edu.vn/css/style.css?v=20230717">
+	<script src="https://www.hvu.edu.vn/js/animatedcollapse.js"></script>
+	<script src="https://www.hvu.edu.vn/js/vquery.js"></script>
 </head>
 
 <body>
